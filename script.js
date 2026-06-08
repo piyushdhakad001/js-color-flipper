f6b4f0ca4f9c4bb9c0d2570a3c71918541413dc8
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-08 12:09:42 +0530

    Add random color generator

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const getRandomRgbColor = () => {
- 
+ const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 
