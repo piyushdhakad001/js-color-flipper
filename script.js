510ccd0d2bddf6f3a134d2f87c1d9bb0d4a9d34e
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-09 21:04:04 +0530

    Improve  code readability

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const getRandomRgbColor = () => {
- const r = Math.floor(Math.random() * 255);
+  const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   return `rgb(${r}, ${g}, ${b})`;
 };
-
 
 const buttonElement = document.querySelector(".js-click-button");
 const bodyElement = document.querySelector(".body");
@@ -12,21 +11,18 @@ const backgroundColorTextElement = document.querySelector(
   ".js-background-color-text",
 );
 
-const savedColor = localStorage.getItem('bgColor');
-if(savedColor){
+const savedColor = localStorage.getItem("bgColor");
+if (savedColor) {
   bodyElement.style.backgroundColor = savedColor;
 
   backgroundColorTextElement.innerHTML = `Background-color: ${savedColor}`;
 }
-
 
 buttonElement.addEventListener("click", (clickColor) => {
   const color = `${getRandomRgbColor()}`;
   bodyElement.style.backgroundColor = color;
 
   backgroundColorTextElement.innerHTML = `Background-color: ${color}`;
-  
-  localStorage.setItem('bgColor', color);
+
+  localStorage.setItem("bgColor", color);
 });
-
-
